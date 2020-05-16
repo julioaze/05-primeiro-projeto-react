488a4e257292c60a4baa901270d7fae53235da02
julioaze <julioaze@gmail.com>
2020-05-16 11:09:21 -0300

    Atualizações

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 // React FC = Function Component
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/5478007?s=460&u=b5eaf8e6aaaa919bf141c97dad6e7d20330807ed&v=4"
+            alt="Julio Azevedo"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/5478007?s=460&u=b5eaf8e6aaaa919bf141c97dad6e7d20330807ed&v=4"
+            alt="Julio Azevedo"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/5478007?s=460&u=b5eaf8e6aaaa919bf141c97dad6e7d20330807ed&v=4"
+            alt="Julio Azevedo"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
